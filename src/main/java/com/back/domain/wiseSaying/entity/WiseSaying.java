3f153e7c9b905d890363bd0d84df52dc0b63812b
chacha86 <taejincha0619@gmail.com>
2026-03-04 15:23:26 +0900

    22

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -2,17 +2,28 @@ package com.back.domain.wiseSaying.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 public class WiseSaying {
 
+    @Setter
     private int id;
     private String content;
     private String author;
 
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
     public void update(String content, String author) {
         this.content = content;
         this.author = author;
+    }
+
+    public boolean isNew() {
+        return this.id == 0;
     }
 }
